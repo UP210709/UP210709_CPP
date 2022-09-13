13cc581f30d5f15c188c0aebe58c69ba049e2611
UP210709 <up210709@alumnos.upa.edu.mx>
2022-09-13 13:26:12 -0500

    Mover al folder

--- Hola mundo.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-    cout<<"Hola mundo :3"<<endl;
-    system("pause");
-    return 0;
-}
--- ParImpar.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-    cout<<" Par o impar "<<"\n"<<endl;
-    system("pause");
-    return 0;
-}
