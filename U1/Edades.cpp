76c39018df215f540abac0c0bc542fc4b8e5d14d
UP210709 <up210709@alumnos.upa.edu.mx>
2022-09-23 14:55:07 -0500

    edad

---
+++ U1/Edades.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int edad;
+    cout<<"Rango de edades \n";
+    cout<<"Ingresa tu edad: ";
+    cin>>edad;
+    if(edad>0 && edad<151)
+    {
+        if (edad<31)
+        {
+            cout<<"Primera edad";
+        }
+        else
+        {
+            if (edad<61)
+            {
+                cout<<"Segunda edad";    
+            }
+            else
+            {
+                if (edad<91)
+                {
+                    cout<<"Tercera edad";
+                }
+                else
+                {
+                    cout<<"Horas extras :3";
+                }
+            }
+            
+        }
+        
+    }
+    else
+    {
+        cout<<"La edad no esta dentro del rango permitido";
+    }
+    return 0;
+}
