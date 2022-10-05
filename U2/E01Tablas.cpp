ed80cee3247330138f722f23d0834e6484de002a
UP210709 <up210709@alumnos.upa.edu.mx>
2022-10-05 11:38:21 -0500

    Ciclos

--- U2/E01Tablas.cpp
+++ U2/E01Tablas.cpp
@@ -2,10 +2,30 @@
 using namespace std;
 int main(){
     int n=5;
-    cout<<"Tablas de multiplicar \n";
-    for (int i = 1; i <= 10; i++)
+    int i;
+    cout<<"Tabla de multiplicar hecha con for \n";
+    for (i = 1; i <= 10; i++)
     {
         cout<<n<<" * "<<i<<" = "<<n*i<<endl;
     }
+    cout<<"\nTabla de multiplicar de manera decreciente \n";
+    for (i = 10; i > 0; i--)
+    {
+        cout<<n<<" * "<<i<<" = "<<n*i<<endl;
+    }
+    cout<<"\nTabla de multiplicar hecha con while \n";
+    i=1;
+    while(i<=10){
+        cout<<n<<" * "<<i<<" = "<<n*i<<endl;
+        i++;
+    }
+    cout<<"\nTabla de multiplicar hecha con do while \n";
+    i=1;
+    do
+    {
+        cout<<n<<" * "<<i<<" = "<<n*i<<endl;
+        i++;
+    } while (i<=10);
+    
     return 0;
 }
